d20a3cfb2afd55aa01ac8e305fb0e82f4c2f53c4
zzes50708 <zzes50708@gmail.com>
2025-12-22 10:39:36 +0800

    Fix Vercel entry point structure

--- api/index.js
+++ api/index.js
@@ -1,2 +1,5 @@
+// 引用原本的 server.js
 const app = require('../server/server.js');
+
+// 匯出 app 讓 Vercel 執行
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -19,12 +19,13 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// 僅在本地開發時啟動 Port 監聽 (Vercel 會自動處理請求，不需要 app.listen)
+// 【修改處】僅在「非」模組引用時（也就是本地執行 node server/server.js 時）才啟動監聽
+// Vercel 環境中，這個檔案是被 require 的，所以不會執行這裡，避免佔用 Port 導致錯誤
 if (require.main === module) {
     app.listen(PORT, () => {
         console.log(`Server running at http://localhost:${PORT}`);
     });
 }
 
-// 必須匯出 app 供 Vercel (api/index.js) 使用
+// 【新增處】必須匯出 app，讓 api/index.js 可以接收
 module.exports = app;
